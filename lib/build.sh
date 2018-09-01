1b53a9eebdafff352ca518c4b6ad750c35161f23
Damian Romanów <damian.romanow@gmail.com>
2018-09-01 11:30:01 +0200

    dont enter asseets

--- lib/build.sh
+++ lib/build.sh
@@ -177,7 +177,7 @@ run_compile() {
   #   rsync -a -v --ignore-existing $cache_dir/phoenix-static/ priv/static
   # fi
 
-  cd $assets_dir
+  # cd $assets_dir
 
   if [ -f $custom_compile ]; then
     info "Running custom compile"
